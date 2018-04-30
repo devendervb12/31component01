5989876703b49e959421fa1f596c5b4a75113a19
devendervb12 <you@example.com>
2018-04-30 08:16:19 -0700

    component

---
+++ WebContent/Component.js
@@ -0,0 +1,18 @@
+
+sap.ui.core.UIComponent.extend("smax.batch31.A1.Component", {
+	
+	createContent : function(){
+		
+		
+		
+		var oApp = sap.ui.view({
+			id : "idApp",
+			viewName : "smax.batch31.A1.view.App",
+			type : "XML"
+		});
+		
+		
+		
+		return oApp;
+	}
+});
