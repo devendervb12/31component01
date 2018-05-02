5481c941548a74cf0332f00b0f7f91376c03dfc1
devendervb12 <you@example.com>
2018-05-02 08:23:02 -0700

    using Router- Navigation

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,18 +1,31 @@
 
 sap.ui.core.UIComponent.extend("smax.batch31.A1.Component", {
 	
-	createContent : function(){
-		
-		
-		
-		var oApp = sap.ui.view({
-			id : "idApp",
-			viewName : "smax.batch31.A1.view.App",
-			type : "XML"
-		});
-		
-		
-		
-		return oApp;
+	metadata : {
+		rootView : "smax.batch31.A1.view.App",
+		routing : {
+			config : {
+				routerClass : "sap.m.routing.Router",			
+				controlId : "idApp",
+				controlAggregation : "pages",
+				viewType : "XML"
+			},
+			routes : [{
+				pattern : "",
+				name : "page1",
+				viewName : "smax.batch31.A1.view.Page1"				
+			},{
+				pattern : "page2pp",
+				name : "page2",
+				viewName : "smax.batch31.A1.view.Page2"
+			}
+			
+			]
+		}
+	},
+	init : function(){
+		sap.ui.core.UIComponent.prototype.init.apply(this);
+		var oRouter = this.getRouter();
+		oRouter.initialize();
 	}
 });
--- WebContent/controller/Page1.controller.js
+++ WebContent/controller/Page1.controller.js
@@ -1,4 +1,4 @@
-sap.ui.controller("view.Page1", {
+sap.ui.controller("smax.batch31.A1.controller.Page1", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
@@ -34,5 +34,8 @@ sap.ui.controller("view.Page1", {
 //	onExit: function() {
 //
 //	}
+	gotoPage2 : function(){
+		this.getOwnerComponent().getRouter().navTo("page2");
+	}
 
 });
